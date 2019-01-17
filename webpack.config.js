5df3d6f89a7c1fbad7f0eadd3108cd7831ad1428
Edgar Quintana <edgarquintana984@hotmail.com>
2019-01-17 09:50:01 -0500

    Adds som webpack.config.js comments

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,23 @@ module.exports = {
     filename: './public/bundle.js'
   },
   resolve: {
+    // Improve working with components
     alias: {
       // Greeter: path.resolve(__dirname, 'public/components/Greeter.js')
     },
+    // The typw of files we are going to use
     extensions: ['*', '.js', '.jsx']
   },
   module: {
+    //LOADERS
+    //Out of the box, webpack only understands JavaScript and JSON files. Loaders allow webpack to process other types of files and convert them into valid modules that can be consumed by your application and added to the dependency graph.
     rules: [
       {
+        // Sets the expected files --> End up with js|jsx
         test: /\.(js|jsx)$/,
+        // Exclude those folders
         exclude: /(node_modules|bower_components)/,
+        // Use this loader and its options
         use: [{
           loader: 'babel-loader',
           options: {
